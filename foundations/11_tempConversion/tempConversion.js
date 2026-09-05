a46ea8881e9e340d306c5b0238014588c27640e5
brian <bpark4684@gmail.com>
2026-09-05 08:38:36 -0400

    Implemented tempConversion functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const FAHRENHEIT_FREEZING_POINT = 32;
+
+const convertToCelsius = function(temp) {
+   	  return Math.round(((temp - FAHRENHEIT_FREEZING_POINT) * 5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(((9 / 5  * temp) + FAHRENHEIT_FREEZING_POINT )* 10) / 10;
 };
 
 // Do not edit below this line
