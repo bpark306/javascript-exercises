6865ffbfea7c899838f7ea1c5727a631a68f5813
brian <bpark4684@gmail.com>
2026-09-05 07:02:53 -0400

    cleaning

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,6 +2,7 @@ const sumAll = function(a, b) {
     if (a < 0 || b < 0) return 'ERROR';
     if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
     if (Number.isNaN(a) || Number.isNaN(b)) return 'ERROR';
+    
     const min = Math.min(a, b)
     const max = Math.max(a, b)
     return (max - min + 1) * (min + max) / 2;
