5fe82a12184351c294ed25bdfd10bbe508522721
brian <bpark4684@gmail.com>
2026-09-05 06:19:49 -0400

    Implemented function reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    result = "";
+    for (let idx = string.length - 1; idx >= 0; idx--) {
+        result += string[idx];
+    }
+    return result;
 };
 
 // Do not edit below this line
