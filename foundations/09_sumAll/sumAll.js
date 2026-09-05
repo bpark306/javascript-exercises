0db0de6e936672d370ccddf8eda0e5dcb7cb208a
brian <bpark4684@gmail.com>
2026-09-05 07:01:19 -0400

    Implemented Gauss version of sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (Number.isNaN(a) || Number.isNaN(b)) return 'ERROR';
+    const min = Math.min(a, b)
+    const max = Math.max(a, b)
+    return (max - min + 1) * (min + max) / 2;
 };
 
 // Do not edit below this line
