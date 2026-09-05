3320169f8d62901ed511a2a8fcef78f862274347
brian <bpark4684@gmail.com>
2026-09-05 06:33:54 -0400

    Implemented function removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    const param = new Set(nums);
+    for (let idx = array.length - 1; idx >= 0; idx--) {
+        if (param.has(array[idx])) {
+            array.splice(idx, 1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
